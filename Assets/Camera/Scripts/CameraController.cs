841fd3363ad54fd8f6feffb3a2b94d54617fb735
Doğukan Büyükarslan <dbabuyukarslan@gmail.com>
2025-03-04 02:08:29 +0300

    Camera transition bug fixed

--- Assets/Camera/Scripts/CameraController.cs
+++ Assets/Camera/Scripts/CameraController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Cinemachine;
 using NaughtyAttributes;
 using UnityEngine;
+using Zenject;
 
 public class CameraController : MonoBehaviour
 {
@@ -19,4 +20,26 @@ public class CameraController : MonoBehaviour
     [SerializeField, Foldout("Setup")] private CinemachineVirtualCamera virtualCamera;
 
     public CameraType Type => cameraType;
+
+    private Vector3 defaultPosition;
+
+    [Inject]
+    private void Construct()
+    {
+        GameManager.OnGameStateChange += OnGameStateChange;
+        defaultPosition = transform.position;
+    }
+
+    private void OnDestroy()
+    {
+        GameManager.OnGameStateChange -= OnGameStateChange;
+    }
+
+    private void OnGameStateChange(GameManager.GameState gameState)
+    {
+        if (gameState == GameManager.GameState.PrepareLevel)
+        {
+            transform.position = defaultPosition;
+        }
+    }
 }
